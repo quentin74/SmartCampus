eb144999f96c40d268179c285ab74831fe1c1ac4
Sup3rN4ch0 <je.hammerer@gmail.com>
2016-03-16 11:53:00 +0100

    Petite Correction

--- load_services/load_services.sh
+++ load_services/load_services.sh
@@ -51,7 +51,7 @@ fi
 
 scp $SHORTCUT create_mosquitto.sh ${USER_MOSQUITTO_SERVER}@${HOST_MOSQUITTO}:~/
 scp $SHORTCUT mosquitto.conf ${USER_MOSQUITTO_SERVER}@${HOST_MOSQUITTO}:~/
-ssh $SHORTCUT ${USER_MOSQUITTO_SERVER}@${HOST_MOSQUITTO} ~/create_mosquitto.sh $CA_KEY_FILE
+ssh $SHORTCUT ${USER_MOSQUITTO_SERVER}@${HOST_MOSQUITTO} ~/create_mosquitto.sh $USER_CA_SERVER $HOST_CA $CA_KEY_FILE
 
 # TELEGRAF setup
 echo 'Telegraf setup on the server :' $HOST_TELEGRAF
@@ -72,7 +72,7 @@ fi
 scp $SHORTCUT create_telegraf.sh ${USER_TELEGRAF_SERVER}@${HOST_TELEGRAF}:~/
 scp $SHORTCUT mqtt_influxDB.conf ${USER_TELEGRAF_SERVER}@${HOST_TELEGRAF}:~/
 scp $SHORTCUT telegraf ${USER_TELEGRAF_SERVER}@${HOST_TELEGRAF}:~/
-ssh $SHORTCUT ${USER_TELEGRAF_SERVER}@${HOST_TELEGRAF} ~/create_telegraf.sh $CA_KEY_FILE
+ssh $SHORTCUT ${USER_TELEGRAF_SERVER}@${HOST_TELEGRAF} ~/create_telegraf.sh $USER_CA_SERVER $HOST_CA $CA_KEY_FILE
 
 # INFLUXDB setup
 echo 'InfluxDB setup on the server :' $HOST_INFLUXDB
@@ -92,7 +92,7 @@ fi
 
 scp $SHORTCUT create_influxDB.sh ${USER_INFLUXDB_SERVER}@${HOST_INFLUXDB}:~/
 scp $SHORTCUT influxdb.conf ${USER_INFLUXDB_SERVER}@${HOST_INFLUXDB}:~/
-ssh $SHORTCUT ${USER_INFLUXDB_SERVER}@${HOST_INFLUXDB} ~/create_influxDB.sh $CA_KEY_FILE
+ssh $SHORTCUT ${USER_INFLUXDB_SERVER}@${HOST_INFLUXDB} ~/create_influxDB.sh $USER_CA_SERVER $HOST_CA $CA_KEY_FILE
 
 # GRAFANA setup
 echo 'Grafana setup on the server :' $HOST_GRAFANA
@@ -103,6 +103,7 @@ else
     SHORTCUT=""
 fi
 
+# Envoi de la clef privée pour se connecter au CA
 if [ -e ${PATH_PRIVATE_KEY_CA} ]
 then
 ssh $SHORTCUT ${USER_GRAFANA_SERVER}@${HOST_GRAFANA} 'mkdir -p ~/.ssh/ca'
@@ -111,4 +112,4 @@ fi
 
 scp $SHORTCUT create_grafana.sh ${USER_GRAFANA_SERVER}@${HOST_GRAFANA}:~/
 scp $SHORTCUT grafana.ini ${USER_GRAFANA_SERVER}@${HOST_GRAFANA}:~/
-ssh $SHORTCUT ${USER_GRAFANA_SERVER}@${HOST_GRAFANA} ~/create_grafana.sh $CA_KEY_FILE
+ssh $SHORTCUT ${USER_GRAFANA_SERVER}@${HOST_GRAFANA} ~/create_grafana.sh $USER_CA_SERVER $HOST_CA $CA_KEY_FILE
